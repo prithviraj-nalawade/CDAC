995f372c97da4167f032f2a744d1353b19f6ad31
Prithviraj Nalawade <75968727+prithviraj-nalawade@users.noreply.github.com>
2024-08-25 15:06:20 +0530

    Add files via upload

---
+++ Logic Building/Assignments/Assignment_1/Rect.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Rect{
+public static void main(String args[]){
+   int a, b;
+   Scanner s = new Scanner(System.in);
+   System.out.println("Enter Length:");
+   a = s.nextInt();
+   System.out.println("Enter Width:");
+   b = s.nextInt();
+     System.out.println("Area of Rectangle:"+(a*b)); 
+   
+}
+}
