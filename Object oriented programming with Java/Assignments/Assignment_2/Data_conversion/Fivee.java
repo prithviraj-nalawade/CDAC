e5740901874b9d0e99818463c09295e0579c5a6a
Prithviraj Nalawade <75968727+prithviraj-nalawade@users.noreply.github.com>
2024-09-06 08:13:27 +0530

    Add files via upload

---
+++ Object oriented programming with Java/Assignments/Assignment_2/Data_conversion/Fivee.java
@@ -0,0 +1,7 @@
+public class Fivee {
+    public static void main(String[] args) {
+        String str = "12345";
+        long i = Long.parseLong(str);
+        System.out.println(i);
+    }
+}
---
+++ Object oriented programming with Java/Assignments/Assignment_2/Data_conversion/Fiveh.java
@@ -0,0 +1,7 @@
+public class Fiveh {
+    public static void main(String[] args) {
+        String str = "13223";
+        Long i = Long.valueOf(str);
+        System.out.println(i);
+    }
+}
