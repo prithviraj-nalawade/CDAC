5e186f42b6bb715967e7ad64335050b9e4b6a94b
Prithviraj Nalawade <75968727+prithviraj-nalawade@users.noreply.github.com>
2024-09-16 11:44:44 +0530

    Add files via upload

---
+++ Object oriented programming with Java/Assignments/Assignment_2/Data_conversion/Ten.java
@@ -0,0 +1,21 @@
+public class Ten {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        String opr = args[0];
+        System.out.println(opr);
+        switch (opr) {
+            case "+": System.out.println("Addition  :   "+Integer.sum(num1, num2));
+                      break;
+            case "-": System.out.println("Substraction  :   "+(num1 -num2));
+                      break;
+            case "*": System.out.println("Multiplication  :   "+(num1 * num2));
+                      break;
+            case "/": System.out.println("Division  :   "+(num1 / num2));
+                      break;
+            default: System.out.println("Invalid operator..");
+                break;
+        }
+
+    }
+}
